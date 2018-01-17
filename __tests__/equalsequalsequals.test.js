979cc77f4cfb6d5dce12fde94c74e29b9a052b73
Christian Dreier <dreier.christian@googlemail.com>
2018-01-17 22:57:03 +0100

    first puzzle

--- __tests__/equalsequalsequals.test.js
+++ __tests__/equalsequalsequals.test.js
@@ -1,8 +1,23 @@
-import underTest from '../equalsequalsequals'
-
 describe("equals equals equals", () => {
-  it("checks a == 1 && etc..", () => {
-    const test = underTest == 1 && underTest == 2 && underTest == 3
-    expect(test).toBeTrue()
+
+  beforeEach(() => {
+    jest.resetModules()
   })
+
+  it("checks a == 1", () => {
+    const underTest = require('../equalsequalsequals').default
+    const test = underTest == 1
+    expect(test).toBe(true)
+  })
+  it("checks a == 1 && a == 2", () => {
+    const underTest = require('../equalsequalsequals').default
+    const test = underTest == 1 && underTest == 2
+    expect(test).toBe(true)
+  })
+  it("checks a == 1 && etc..", () => {
+    const underTest = require('../equalsequalsequals').default
+    const test = underTest == 1 && underTest == 2 && underTest == 3
+    expect(test).toBe(true)
+  })
+
 })
