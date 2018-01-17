38ec87ee32041c7201adddb8384fe4475139814d
Christian Dreier <dreier.christian@googlemail.com>
2018-01-17 23:01:07 +0100

    added readme

---
+++ README.md
@@ -0,0 +1,7 @@
+# javascript puzzles!
+
+run `npm install`
+
+and `npm run testw` to start the testwatcher
+
+try to pass all the tests!
--- __tests__/equalsequalsequals.test.js
+++ __tests__/equalsequalsequals.test.js
@@ -14,7 +14,7 @@ describe("equals equals equals", () => {
     const test = underTest == 1 && underTest == 2
     expect(test).toBe(true)
   })
-  it("checks a == 1 && etc..", () => {
+  it("checks a == 1 && a == 2 && a == 3", () => {
     const underTest = require('../equalsequalsequals').default
     const test = underTest == 1 && underTest == 2 && underTest == 3
     expect(test).toBe(true)
